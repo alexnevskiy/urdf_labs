2f2f30cc0e760d8833050dda697bd34854888ea0
Aleksandr Kobyzhev <aleksandr.kobyzhev@spbpu.com>
2023-11-18 20:25:08 +0300

    Add lab2

---
+++ build/myrobot/myrobot_control/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/alexnevskiy/catkin_ws/src/myrobot/myrobot_control
+# Build directory: /home/alexnevskiy/catkin_ws/build/myrobot/myrobot_control
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
